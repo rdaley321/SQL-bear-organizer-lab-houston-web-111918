01b0e8fa80e61b0c063883a38de0c48e8dbb88e7
Ryan Daley <rdaley321@yahoo.com>
2018-11-26 23:14:36 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",1,"male","brown","salty",1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Rowdy",12,"male","black","sad",1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Tabitha",3,"female","brown","salty",0);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",2,"male","black","happy",1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Melissa",9,"female","brown","happy",0);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Grinch",12,"male","brown","grump",1);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES ("Wendy",5,"female","black","sad",0);
+INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES (NULL,8,"male","brown","happy",1);
